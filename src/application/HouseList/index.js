a756bb9bd328aec0d083f5662766c389da419d23
Dandelion-F <2604746157@qq.com>
2022-05-23 22:11:55 +0800

    feat: 引入房屋卡片列表组件

--- src/application/HouseList/index.js
+++ src/application/HouseList/index.js
@@ -1,9 +1,12 @@
 import React from "react";
+import HouseCards from "./components/HouseCards";
+import "./style.css";
 
 function HouseList(props) {
   return (
     <>
       <div>房屋列表k</div>
+      <HouseCards></HouseCards>
     </>
   );
 }
