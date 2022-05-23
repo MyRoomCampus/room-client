69c8ab0d9ecd0e2d9b4b185b351c95a63a33066f
Dandelion-F <2604746157@qq.com>
2022-05-23 22:11:23 +0800

    feat: 添加房屋卡片列表（简版）

---
+++ src/application/HouseList/components/HouseCards/HouseCard.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Row, Col, Card } from '@douyinfe/semi-ui';
+import { Link } from 'react-router-dom';
+
+export default function HomeCard() {
+  return (
+    <Row gutter={[16, 16]}>
+      <Col xs={24} lg={{ span: 12, offset: 6 }}>
+        <Link to="/houseDetails">
+          <Card title="碧桂园" bordered={false}>
+            三室一厅
+          </Card>
+        </Link>
+      </Col>
+    </Row>
+  );
+}
---
+++ src/application/HouseList/components/HouseCards/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import HouseCard from './HouseCard';
+
+export default function HomeCards() {
+  return (
+    <>
+      <HouseCard></HouseCard>
+    </>
+  );
+}
