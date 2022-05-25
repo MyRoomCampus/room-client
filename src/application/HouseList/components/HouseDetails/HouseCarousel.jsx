0a3a0443c7aa5b0e6bb4da6928d42acb5a303340
Dandelion-F <2604746157@qq.com>
2022-05-25 17:32:15 +0800

    fix: 修改轮播图组件

--- src/application/HouseList/components/HouseDetails/HouseCarousel.jsx
+++ src/application/HouseList/components/HouseDetails/HouseCarousel.jsx
@@ -1,31 +1,11 @@
 import React from 'react';
-import { Carousel, Space, Typography } from '@douyinfe/semi-ui';
+import { Carousel } from '@douyinfe/semi-ui';
 
 export default class HouseCarousel extends React.Component {
   constructor(props) {
     super(props);
-    this.imgList = [
-      'https://lf3-static.bytednsdoc.com/obj/eden-cn/hjeh7pldnulm/SemiDocs/bg-1.png',
-      'https://lf3-static.bytednsdoc.com/obj/eden-cn/hjeh7pldnulm/SemiDocs/bg-2.png',
-      'https://lf3-static.bytednsdoc.com/obj/eden-cn/hjeh7pldnulm/SemiDocs/bg-3.png',
-    ];
-    this.textList = [
-      [
-        'Semi 设计管理系统',
-        '从 Semi Design，到 Any Design',
-        '快速定制你的设计系统，并应用在设计稿和代码中',
-      ],
-      [
-        'Semi 物料市场',
-        '面向业务场景的定制化组件，支持线上预览和调试',
-        '内容由 Semi Design 用户共建',
-      ],
-      [
-        'Semi Pro (开发中)',
-        '基于 40+ 真实组件代码设计',
-        '海量页面模板前端代码一键转',
-      ],
-    ];
+    this.imgList = ['/house-1.jpg', '/house-2.jpg', '/house-3.jpg'];
+    this.textList = [];
     this.state = {
       activeIndex: 0,
     };
@@ -51,16 +31,6 @@ export default class HouseCarousel extends React.Component {
       height: '400px',
     };
 
-    const titleStyle = {
-      position: 'absolute',
-      top: '100px',
-      left: '100px',
-    };
-
-    const colorStyle = {
-      color: '#1C1F23',
-    };
-
     const { activeIndex } = this.state;
 
     return (
@@ -80,27 +50,7 @@ export default class HouseCarousel extends React.Component {
                   backgroundSize: 'cover',
                   backgroundImage: `url(${src})`,
                 }}
-              >
-                <Space
-                  vertical
-                  align="start"
-                  spacing="medium"
-                  style={titleStyle}
-                >
-                  {this.renderLogo()}
-                  <Typography.Title heading={2} style={colorStyle}>
-                    {this.textList[index][0]}
-                  </Typography.Title>
-                  <Space vertical align="start">
-                    <Typography.Paragraph style={colorStyle}>
-                      {this.textList[index][1]}
-                    </Typography.Paragraph>
-                    <Typography.Paragraph style={colorStyle}>
-                      {this.textList[index][2]}
-                    </Typography.Paragraph>
-                  </Space>
-                </Space>
-              </div>
+              ></div>
             );
           })}
         </Carousel>
