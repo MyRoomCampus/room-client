2137ade4594f6e667e2b82df18cc24dba45ce7b3
Dandelion-F <2604746157@qq.com>
2022-05-27 11:16:47 +0800

    feat: 添加 fake 数据

---
+++ public/fakeData/houseInfo.js
@@ -0,0 +1,27 @@
+const houseInfo = {
+    id: 1,
+    referImages: ["", "", ""],
+    envTags: ["楼龄新", "近地铁"],
+    descTags: ["3室2厅", "远洋山水"],
+    qualifications: ["房源发布机构", "相关资质"],
+    price: "190万",
+    descType: "3室2厅",
+    houseArea: "108.99平",
+    unitPrice: "17592元/平",
+    publishDate: "2020-12-23",
+    decoration: "精装修",
+    elevator: "有",
+    type: "普通住宅",
+    buildTime: "2019年",
+    address: "西三旗 富力桃园",
+    budget: "计算贷款金额",
+    floorNum: 18,
+    verifyCode: "20210112hAEG7",
+    manager: {
+        id: 1
+    },
+    articles: [1, 2, 3],
+
+};
+
+export default houseInfo;
---
+++ public/fakeData/manager.js
@@ -0,0 +1,13 @@
+const manager = {
+    id: 1,
+    avatar: "",
+    name: "李梦",
+    tags: ["我爱我家"],
+    serviceGrade: 3.7,
+    familiarityOfArea: 2.3,
+    commentTags: ["服务标杆", "闪电回复"],
+    sendMsg: "",
+    phoneCall: "",
+};
+
+export default manager;
