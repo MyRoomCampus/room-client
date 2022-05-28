7cc153fca725a78bc987996b8385ad6fc654512f
Dandelion-F <2604746157@qq.com>
2022-05-28 21:30:16 +0800

    feat: js 适配 semi 夜间模式

--- src/index.js
+++ src/index.js
@@ -4,6 +4,26 @@ import 'reset-css'
 import '../src/assets/global-style.css';
 import App from './App';
 
+if (window.matchMedia) {
+  const mql = window.matchMedia('(prefers-color-scheme: dark)');
+
+  function matchMode(e) {
+    const body = document.body;
+    if (e.matches) {
+      if (!body.hasAttribute('theme-mode')) {
+        body.setAttribute('theme-mode', 'dark');
+      }
+    } else {
+      if (body.hasAttribute('theme-mode')) {
+        body.removeAttribute('theme-mode');
+      }
+    }
+  }
+
+  mql.addListener(matchMode);
+  matchMode(mql)
+}
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
