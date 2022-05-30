46b2fae4cd8417a8ca43eca8fa6f3a236ec94f5a
Dandelion-F <2604746157@qq.com>
2022-05-30 22:41:49 +0800

    feat: 添加房屋详情接口

--- src/api/request.js
+++ src/api/request.js
@@ -3,3 +3,7 @@ import { axiosInstance } from "./config";
 export const getMusicList = () => {
   return axiosInstance.get("/user/record?uid=45441555&type=1");
 };
+
+export const getHouseInfo = (houseId) => {
+  return axiosInstance.get(`/house/${houseId}`);
+}
