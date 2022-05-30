29e948af68c0c92773f6748696715018934b380e
Dandelion-F <2604746157@qq.com>
2022-05-30 22:38:38 +0800

    fix: 接入线上详情接口 & 格式化响应(camelCase -> snake_case)

--- src/application/HouseList/components/HouseDetails/index.jsx
+++ src/application/HouseList/components/HouseDetails/index.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Col, Row } from '@douyinfe/semi-ui';
-import { getHouseInfo } from '../../../../fakeData';
+import { getHouseInfo } from '../../../../api/request';
 import { useRequest } from 'ahooks';
 import HouseCarousel from './HouseCarousel';
 import HouseData from './HouseData';
@@ -12,9 +12,28 @@ import HousePrice from './HousePrice';
 import HouseSource from './HouseSource';
 import HouseNeighborSource from './HouseNeighborSource';
 import HouseFooter from './HouseFooter';
+import { useParams } from 'react-router-dom';
+import snakeCase from 'lodash/snakeCase';
 
 export default function HomeDetail() {
-  const { data, loading } = useRequest(getHouseInfo);
+  const params = useParams();
+
+  const { data: _data, loading } = useRequest(() => getHouseInfo(params.id), {
+    refreshDeps: [params.id],
+    loadingDelay: 100,
+  });
+
+  const data = _data?.data
+    ? Object.fromEntries(
+        Object.entries(_data?.data).map(([k, v]) => [snakeCase(k), v])
+      )
+    : null;
+
+  useEffect(() => {
+    setTimeout(() => {
+      document.body.scrollTop = document.documentElement.scrollTop = 0;
+    }, 100);
+  }, [params.id]);
 
   return (
     <Row>
