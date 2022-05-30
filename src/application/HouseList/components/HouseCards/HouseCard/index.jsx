a361f809c030e447e7c2e6eb67e43a71ef9fa8af
Dandelion-F <2604746157@qq.com>
2022-05-30 22:40:14 +0800

    feat: 房屋卡片路由传参

--- src/application/HouseList/components/HouseCards/HouseCard/index.jsx
+++ src/application/HouseList/components/HouseCards/HouseCard/index.jsx
@@ -3,11 +3,12 @@ import { Row, Col, Card } from '@douyinfe/semi-ui';
 import { Link } from 'react-router-dom';
 import './style.css';
 
-export default function HomeCard() {
+export default function HomeCard(props) {
+  const houseId = props.id || '65140';
   return (
     <Row gutter={[16, 16]}>
       <Col xs={24} lg={{ span: 12, offset: 6 }}>
-        <Link to="/houseDetails">
+        <Link to={`/houseDetails/${houseId}`}>
           <Card title="碧桂园" bordered={false}>
             三室一厅
           </Card>
