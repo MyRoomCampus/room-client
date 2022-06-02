b4dedf5b17b96141d1b958152e1382fe70376eaa
Dandelion-F <2604746157@qq.com>
2022-06-02 15:33:49 +0800

    feat: 详情页添加返回&回到顶部

--- src/application/HouseDetails/index.jsx
+++ src/application/HouseDetails/index.jsx
@@ -1,6 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import { Col, Row, Button } from '@douyinfe/semi-ui';
+import React, { useEffect } from 'react';
+import { Col, Row, BackTop } from '@douyinfe/semi-ui';
+import { IconArrowLeft, IconArrowUp } from '@douyinfe/semi-icons';
 import { getHouseInfo } from '../../api/request';
+import { useRequest } from 'ahooks';
 import HouseCarousel from './HouseCarousel';
 import HouseData from './HouseData';
 import HouseManager from './HouseManager';
@@ -36,65 +38,81 @@ if (window.matchMedia) {
   matchMode(mql);
 }
 
+const arrowCommonStyle = {
+  backgroundColor: '#fff',
+  width: 30,
+  height: 30,
+  borderRadius: 15,
+  lineHeight: '30px',
+  boxShadow: '0px 10px 15px -3px rgba(0,0,0,0.1)',
+};
+
 export default function HomeDetail(props) {
   let { pid } = props;
-  let { id } = useParams();
-  if (pid !== undefined) {
-    id = pid;
-  }
+  let { id: _id } = useParams();
+  const id = pid ?? _id;
 
-  const [houseDetails, setHouseDetails] = useState({});
-  const [loading, setLoading] = useState(true);
-  const getHouseDetails = (id) => {
-    getHouseInfo(id)
-      .then((response) => {
-        // console.log(response);
-        setHouseDetails(response.data.data);
-        setLoading(false);
-        // console.log("---");
-        // console.log(houseDetails);
-      })
-      .catch((e) => {
-        console.log(e);
-      });
-  };
+  const { data: _data, loading } = useRequest(() => getHouseInfo(id), {
+    refreshDeps: [id],
+    loadingDelay: 100,
+  });
+
+  const data = _data?.data?.data;
+
   useEffect(() => {
-    getHouseDetails(id);
+    setTimeout(() => {
+      document.body.scrollTop = document.documentElement.scrollTop = 0;
+    }, 100);
   }, [id]);
-  const path = useLocation().pathname.split("/")[1];
+
+  const path = useLocation().pathname.split('/')[1];
   const navigate = useNavigate();
   const handleNavigate = () => {
-    if (path === "activity") {
-      navigate("/");
-    }
-    else if (path === "houseDetails") {
+    if (path === 'activity') {
+      navigate('/');
+    } else if (path === 'houseDetails') {
       navigate(`/activity/${id}`);
     }
-  }
+  };
 
   return (
-    <>
-      {JSON.stringify(houseDetails) === "{}" ? null : (
-        <div id="top">
-          <div className='back-nav'>
-            <Button onClick={handleNavigate} style={{ height: "50px" }} theme="solid"><span className="iconfont">&#xe8a4;</span></Button>
-          </div>
-          <Row>
-            <Col xs={24} lg={{ span: 12, offset: 6 }}>
-              <HouseCarousel></HouseCarousel>
-              <HouseData data={houseDetails} loading={loading}></HouseData>
-              <HouseManager></HouseManager>
-              <HouseTest></HouseTest>
-              <HouseAssess></HouseAssess>
-              <HouseNeighbor></HouseNeighbor>
-              <HousePrice></HousePrice>
-              <HouseSource></HouseSource>
-              <HouseNeighborSource></HouseNeighborSource>
-              <HouseFooter></HouseFooter>
-            </Col>
-          </Row></div>
-      )
-      }
-    </>
+    <Row className="home-detail-container">
+      <Col xs={24} lg={{ span: 12, offset: 6 }}>
+        <HouseCarousel loading={loading}></HouseCarousel>
+        <div style={{ padding: '0 8px' }}>
+          <HouseData data={data} loading={loading}></HouseData>
+          <HouseManager loading={loading}></HouseManager>
+          <HouseTest loading={loading}></HouseTest>
+          <HouseAssess loading={loading}></HouseAssess>
+          <HouseNeighbor loading={loading}></HouseNeighbor>
+          <HousePrice loading={loading}></HousePrice>
+          <HouseSource loading={loading}></HouseSource>
+          <HouseNeighborSource loading={loading}></HouseNeighborSource>
+        </div>
+        <HouseFooter loading={loading}></HouseFooter>
+      </Col>
+      <BackTop
+        style={{
+          top: 15,
+          left: 10,
+          right: 'auto',
+          bottom: 'auto',
+          ...arrowCommonStyle,
+        }}
+        onClick={handleNavigate}
+        visibilityHeight={-1}
+      >
+        <IconArrowLeft />
+      </BackTop>
+      <BackTop
+        style={{
+          right: 15,
+          bottom: 120,
+          ...arrowCommonStyle,
+        }}
+      >
+        <IconArrowUp />
+      </BackTop>
+    </Row>
   );
 }
