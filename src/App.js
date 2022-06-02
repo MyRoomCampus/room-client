7250317109b051c313c233886f46fccabfde5ac9
magicmooc <riyingxiaozi@gmail.com>
2022-06-02 17:02:17 +0800

    add dark mode

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { useRoutes } from "react-router";
 import { BrowserRouter } from "react-router-dom";
 import routes from "./routes";
 import store from "./store";
-
+import "./style.css";
 const ProjectRouter = () => {
   return useRoutes(routes);
 };
